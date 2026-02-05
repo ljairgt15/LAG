89b58445a4cb927f357a1435d6805f81b92fc49a
ljairgt15 <ludwing.j@hotmail.com>
2026-02-05 16:02:03 -0500

    [Change] Updated correo

--- 01-UpdateTemplateReplaceCustomerByConsignee.sql.sql
+++ 01-UpdateTemplateReplaceCustomerByConsignee.sql.sql
@@ -1,15 +1,18 @@
 /*
-VERSION		MODIFIEDBY			MODIFIEDDATE	HU		MODIFICATION
-1		Jair Gomez			2026-02-04	NEW WORK 57731	Update COSTUMER to CONSIGNEE in subject and body
+VERSION     MODIFIEDBY      MODIFIEDDATE    HU      MODIFICATION
+1           Jair Gomez     2026-02-05      57731   Update COSTUMER to CONSIGNEE in subject and body
 */
+
 IF EXISTS (
     SELECT 1
     FROM [dbo].[OpcionesRetraso] T WITH (NOLOCK)
     WHERE T.Id = 'ORT0140'
-        AND (
-            T.Asunto LIKE '%COSTUMER%'
-            OR T.Descripcion LIKE '%c¿Customer%'
-        )
+      AND (
+            T.Asunto LIKE '%[[COSTUMER]]%'
+         OR T.Asunto LIKE '%Customer%'
+         OR T.Descripcion LIKE '%[[COSTUMER]]%'
+         OR T.Descripcion LIKE '%Customer%'
+      )
 )
 BEGIN
     UPDATE T
@@ -17,12 +20,18 @@ BEGIN
         Asunto = REPLACE(
                     REPLACE(T.Asunto, '[COSTUMER]', '[CONSIGNEE]'),
                     'Customer', 'Consignee'
-                    ),
+                ),
         Descripcion = REPLACE(
-                    REPLACE(T.Descripcion, '[COSTUMER]', '[CONSIGNEE]'),
-                    'Customer', 'Consignee'
-                    ),
+                        REPLACE(T.Descripcion, '[COSTUMER]', '[CONSIGNEE]'),
+                        'Customer', 'Consignee'
+                      ),
         FechaCambio = GETDATE()
     FROM [dbo].[OpcionesRetraso] T
-    WHERE T.Id ='ORT0140'
+    WHERE T.Id = 'ORT0140';
+
+    PRINT 'OpcionesRetraso updated successfully: [COSTUMER]/Customer replaced with CONSIGNEE for ORT0140.'
+END
+ELSE
+BEGIN
+    PRINT 'No update required: [COSTUMER]/Customer not found for ORT0140.'
 END
