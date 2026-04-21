b545a4d70f1d3e6cfcdedbfba61897e1f8149a63
Ludwing Gomez <systems.ext9@logiztikalliance.com>
2026-04-20 20:52:31 -0500

    [Change] Data

--- CODIGOBARRA360/TEST-DATA/Data.sql
+++ CODIGOBARRA360/TEST-DATA/Data.sql
@@ -27,7 +27,7 @@ exec dbo.pro_ConsultarCodigoBarrasClientes
 exec dbo.AC_pro_GetBarCodeExternal 
 	@fechaDesde='20220101',
 	@fechaHasta='20260423',
-	@estado=N'<?xml version="1.0" encoding="utf-16"?>  <ArrayOfString xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">    <string>PENDING</string>  </ArrayOfString>',
+	@estado=N'<?xml version="1.0" encoding="utf-16"?>  <ArrayOfString xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">    <string>PENDING</string>   <string>RECEIVED WH</string> </ArrayOfString>',
 	@isDispatchCarrier=0,
 	@EntityId=N'CLI013680',
 	@consigneeName=N'BOTANICA WHOLESALE HOUSTON',
@@ -36,7 +36,7 @@ exec dbo.AC_pro_GetBarCodeExternal
 exec dbo.pro_ConsultarCodigoBarrasClientes 
 	@fechaDesde='20220101',
 	@fechaHasta='20260423',
-	@estado=N'<?xml version="1.0" encoding="utf-16"?>  <ArrayOfString xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">    <string>PENDING</string>  </ArrayOfString>',
+	@estado=N'<?xml version="1.0" encoding="utf-16"?>  <ArrayOfString xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">    <string>PENDING</string>   <string>RECEIVED WH</string> </ArrayOfString>',
 	@isDispatchCarrier=0,
 	@IdCliente=N'CLI013680',
 	@nombreClienteDistribucion=N'BOTANICA WHOLESALE HOUSTON'
