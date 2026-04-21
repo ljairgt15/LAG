d9421fbc21c09a989cead1213e09632a108f4b7c
ljairgt15 <ludwing.j@hotmail.com>
2026-04-21 10:03:23 -0500

    [Change] TestCase

--- CODIGOBARRA360/AC_pro_GetBarCodeExternal.sql
+++ CODIGOBARRA360/AC_pro_GetBarCodeExternal.sql
@@ -3574,9 +3574,27 @@ BEGIN
 END
 /*
 
-AC_pro_GetBarCodeExternal @ConsigneeId='ETY0000000018053',
-@FechaDespacho= '20260109', @IdBodega='LXgyot5M', @IdCarrier= 'ybOy4oex7F5E',
-@sinManifiesto=1, @EsInventario=0, @FechaDesde='20210409', @FechaHasta= '20260409',
-@EntityId='CLI0127475'
+exec dbo.AC_pro_GetBarCodeExternal 
+	@fechaDesde='20210713',
+	@fechaHasta='20260714',
+	@estado=N'<?xml version="1.0" encoding="utf-16"?>  <ArrayOfString xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">    <string>PENDING</string>  </ArrayOfString>',
+	@idCarrier=N'9Nlyxt0q6dGE',
+	@idBodega=N'LXgyot5M',
+	@fechaDespacho='20260413',
+	@isDispatchCarrier=0,
+	@esInventario=0,
+	@EntityId=N'CLI013680',
+	@shipToId=N'ETY011729',
+	@UserType=N'GRUPOCLIENTE'
+
+exec dbo.AC_pro_GetBarCodeExternal 
+	@fechaDesde='20220101',
+	@fechaHasta='20260423',
+	@estado=N'<?xml version="1.0" encoding="utf-16"?>  <ArrayOfString xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">    <string>PENDING</string>   <string>RECEIVED WH</string> </ArrayOfString>',
+	@isDispatchCarrier=0,
+	@EntityId=N'CLI013680',
+	@consigneeName=N'BOTANICA WHOLESALE HOUSTON',
+	@consigneeId=N'ETY011765',
+	@UserType=N'GRUPOCLIENTE'
 
 */
