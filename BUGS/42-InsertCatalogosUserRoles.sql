ab2c97acdb2e0e1837779393c2f50de4c4e2d4e9
ljairgt15 <ludwing.j@hotmail.com>
2026-05-12 16:20:00 -0500

    [Change] Eliminar cliente y billto

--- BUGS/42-InsertCatalogosUserRoles.sql
+++ BUGS/42-InsertCatalogosUserRoles.sql
@@ -1,9 +1,10 @@
 /*
 VERSION		MODIFIEDBY				MODIFIEDDATE	HU					MODIFICATION
-1			Mateo Velasco			2025-06-25		AC 51314			Initial Code - Creation of Consignees Rol
+1			Mateo Velasco			2025-06-25		55188				Initial Code - Creation of Consignees Rol
 2			Jorge Ortiz			    2025-07-24		LAG-CT-013 53071	Initial Code - Add new Catalog
 3           Ian Carlos Ortega       2025-12-15      LAG-CT-019 55188    Add new role called 'CLIENTE' to UserRoles catalog
-4           Jair Gomez              2026-04-14      LAG-CT-019 55188    Add suffix _TYPE to UserRoles catalog 
+4           Jair Gomez              2026-04-14      LAG-CT-019 55188    Add suffix _TYPE to UserRoles catalog
+5           Jair Gomez              2026-05-10      LAG-CT-033 65319    Eliminate BILLTO AND CONSIGNEE role
 */
 IF NOT EXISTS (
       SELECT 1
@@ -14,8 +15,6 @@ IF NOT EXISTS (
             'BROKER_TYPE',
             'EMPLEADO_TYPE',
             'EXPORTADOR_TYPE',
-            'BILLTO_TYPE',
-            'CONSIGNEE_TYPE',
             'CLIENTE_TYPE'
       )
 )
@@ -196,93 +195,6 @@ BEGIN
 
     SELECT @IdNew = MAX([IdNew]) + 1
     FROM [dbo].[Catalogos];
-    INSERT INTO [dbo].[Catalogos]
-        (
-        [idEmpresa]
-        ,[nombre]
-        ,[nombreIngles]
-        ,[codigo]
-        ,[tipo]
-        ,[descripcion]
-        ,[status]
-        ,[nota]
-        ,[fechaCambio]
-        ,[identificador]
-        ,[clase]
-        ,[codigoRelacion]
-        ,[orden]
-        ,[oculto]
-        ,[idCatalgoPadre]
-        ,[idRegistroVinculado]
-        ,[IdNew]
-        )
-    VALUES
-        (
-        NULL
-        ,'BILL TO'
-        ,'BILL TO'
-        ,'UserRoles'
-        ,'ENUMERACION'
-        ,'User role type for listing'
-        ,'ACTIVO'
-        ,NULL
-        ,GETDATE()
-        ,'BILLTO_TYPE'
-        ,NULL
-        ,NULL
-        ,7
-        ,0
-        ,NULL
-        ,NULL
-        ,@IdNew
-        );
-
-    SELECT @IdNew = MAX([IdNew]) + 1
-    FROM [dbo].[Catalogos];
-
-    INSERT INTO [dbo].[Catalogos]
-        (
-        [idEmpresa]
-        ,[nombre]
-        ,[nombreIngles]
-        ,[codigo]
-        ,[tipo]
-        ,[descripcion]
-        ,[status]
-        ,[nota]
-        ,[fechaCambio]
-        ,[identificador]
-        ,[clase]
-        ,[codigoRelacion]
-        ,[orden]
-        ,[oculto]
-        ,[idCatalgoPadre]
-        ,[idRegistroVinculado]
-        ,[IdNew]
-        )
-    VALUES
-        (
-        NULL
-        ,'CONSIGNATARIOS'
-        ,'CONSIGNEES'
-        ,'UserRoles'
-        ,'ENUMERACION'
-        ,'User role type for listing'
-        ,'ACTIVO'
-        ,NULL
-        ,GETDATE()
-        ,'CONSIGNEE_TYPE'
-        ,NULL
-        ,NULL
-        ,3
-        ,0
-        ,NULL
-        ,NULL,
-        @IdNew
-        );
-
-    SELECT @IdNew = MAX([IdNew]) + 1
-    FROM [dbo].[Catalogos];
 
     INSERT INTO [dbo].[Catalogos]
         (
@@ -318,7 +230,7 @@ BEGIN
         ,'CLIENTE_TYPE'
         ,NULL
         ,NULL
-        ,8
+        ,6
         ,0
         ,NULL
         ,NULL,
