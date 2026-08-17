a33d3fc0c947a4079974192a413d42dfa968494f
ljairgt15 <ludwing.j@hotmail.com>
2026-08-17 12:03:19 -0500

    [Change] Palabras reservadas

--- EDI/03-Ac_pro_PartialDeleteLocalOrders.sql
+++ EDI/03-Ac_pro_PartialDeleteLocalOrders.sql
@@ -4,107 +4,106 @@ VERSION		MODIFIEDBY			MODIFIEDDATE	  HU			 MODIFICATION
 */
 CREATE OR ALTER PROCEDURE [dbo].[AC_pro_PartialDeleteLocalOrders](@ListaCodigosBarras VARCHAR(MAX))
 
-AS 
+AS
 
 BEGIN
 
-DECLARE @table table ( id INT IDENTITY(1,1), codigoBarra varchar(16))
+DECLARE @table TABLE ( id INT IDENTITY(1,1), codigoBarra VARCHAR(16))
 
 INSERT INTO @table(codigoBarra)
-	SELECT codigoBarra
-	FROM OPENJSON(@ListaCodigosBarras)
-	WITH (codigoBarra varchar(32)'$.codigoBarra')
+SELECT codigoBarra
+FROM OPENJSON(@ListaCodigosBarras)
+WITH (codigoBarra VARCHAR(32)'$.codigoBarra')
 
 BEGIN TRY
-	--BORRAR
-	BEGIN TRAN EliminaRegistros
+    --BORRAR
+    BEGIN TRAN EliminaRegistros
 
-	--Guardamos la programacion anterior
-					INSERT INTO DatosProgramacionAnterior (Id,
-														   IdGuiaHouseDetalle,
-														   IdCliente,
-														   NombreCliente,
-														   IdCarrier,
-														   NombreCarrier,
-														   FechaDespacho,
-														   FechaCambio,
-														   IdUsuarioProgramacion,
-														   IdUsuarioLog,
-														   Accion,
-														   Nota)
-					select NEWID(),
-						GHD.id, 
-						GHD.ShipToId as IdCliente,
-						CLI.nombre as NombreCliente,
-						PRO.IdCarrier,
-						TRA.Nombre as NombreCarrier,
-						PRO.FechaDespacho,
-						GETDATE() as FechaCambio,
-						PRO.IdUsuarioLog,
-						NULL,
-						'E',
-						'AC_pro_OrdenesLocales_EliminacionParcial'
-					from GuiasHouseDetalles GHD					
-					INNER JOIN ProgramacionCarrier PRO ON GHD.id = PRO.idGuiaHouseDetalle
-					INNER JOIN v_ClientsEntities CLI ON GHD.ShipToId = CLI.id
-					INNER JOIN Transportes TRA ON PRO.idCarrier = TRA.id
-					INNER JOIN @table tabTemp on  GHD.codigoBarra= tabTemp.codigoBarra;
-	
-	DELETE pc FROM ProgramacionCarrier pc
-	INNER JOIN GuiasHouseDetalles ghd on pc.idGuiaHouseDetalle = ghd.id
-	INNER JOIN @table tabTemp on  ghd.codigoBarra= tabTemp.codigoBarra
+    --Guardamos la programacion anterior
+                    INSERT INTO DatosProgramacionAnterior (Id,
+                                                           IdGuiaHouseDetalle,
+                                                           IdCliente,
+                                                           NombreCliente,
+                                                           IdCarrier,
+                                                           NombreCarrier,
+                                                           FechaDespacho,
+                                                           FechaCambio,
+                                                           IdUsuarioProgramacion,
+                                                           IdUsuarioLog,
+                                                           Accion,
+                                                           Nota)
+                    SELECT NEWID(),
+                        GHD.id,
+                        GHD.ShipToId AS IdCliente,
+                        CLI.nombre AS NombreCliente,
+                        PRO.IdCarrier,
+                        TRA.Nombre AS NombreCarrier,
+                        PRO.FechaDespacho,
+                        GETDATE() AS FechaCambio,
+                        PRO.IdUsuarioLog,
+                        NULL,
+                        'E',
+                        'AC_pro_OrdenesLocales_EliminacionParcial'
+                    FROM GuiasHouseDetalles GHD
+                    INNER JOIN ProgramacionCarrier PRO ON GHD.id = PRO.idGuiaHouseDetalle
+                    INNER JOIN v_ClientsEntities CLI ON GHD.ShipToId = CLI.id
+                    INNER JOIN Transportes TRA ON PRO.idCarrier = TRA.id
+                    INNER JOIN @table tabTemp ON GHD.codigoBarra = tabTemp.codigoBarra;
 
-	DELETE up FROM UbicacionPiezas up
-	inner join GuiasHouseDetalles ghd on up.idGuiaHouseDetalle = ghd.id
-	inner join @table tabTemp on ghd.codigoBarra = tabTemp.codigoBarra
-	
-	--obtencion para encabezados
-	select ghd.idGuiaHouse into #tempGuiaHouse FROM GuiasHouseDetalles ghd
-	inner join @table tabTemp on ghd.codigoBarra = tabTemp.codigoBarra
+    DELETE pc FROM ProgramacionCarrier pc
+    INNER JOIN GuiasHouseDetalles ghd ON pc.idGuiaHouseDetalle = ghd.id
+    INNER JOIN @table tabTemp ON ghd.codigoBarra = tabTemp.codigoBarra
 
-	DELETE ghd FROM GuiasHouseDetalles ghd
-	inner join @table tabTemp on ghd.codigoBarra = tabTemp.codigoBarra
+    DELETE up FROM UbicacionPiezas up
+    INNER JOIN GuiasHouseDetalles ghd ON up.idGuiaHouseDetalle = ghd.id
+    INNER JOIN @table tabTemp ON ghd.codigoBarra = tabTemp.codigoBarra
 
-	--obtencion para poEncabezado
-	select pd.idPo into #tempPoEncabezado FROM PoDetalles pd
-	inner join @table tabTemp on pd.codigoBarra = tabTemp.codigoBarra
+    --obtencion para encabezados
+    SELECT ghd.idGuiaHouse INTO #tempGuiaHouse FROM GuiasHouseDetalles ghd
+    INNER JOIN @table tabTemp ON ghd.codigoBarra = tabTemp.codigoBarra
 
-	select pe.idOrdenLocal into #tempOrdenLocal FROM PoEncabezado pe
-	inner join #tempPoEncabezado tabTemp on pe.id = tabTemp.idPo
-	
-	DELETE pd FROM PoDetalles pd
-	inner join @table tabTemp on pd.codigoBarra = tabTemp.codigoBarra
-	
+    DELETE ghd FROM GuiasHouseDetalles ghd
+    INNER JOIN @table tabTemp ON ghd.codigoBarra = tabTemp.codigoBarra
 
-	--Eliminacion encabezados
-	select pe.id,count(pd.id) as cantidadDetalle into #poEncabezadoDetalle from PoEncabezado pe
-	left join PoDetalles pd on pe.id = pd.idPo
-	inner join #tempPoEncabezado temp on pe.id = temp.idPo
-	group by pe.id
+    --obtencion para poEncabezado
+    SELECT pd.idPo INTO #tempPoEncabezado FROM PoDetalles pd
+    INNER JOIN @table tabTemp ON pd.codigoBarra = tabTemp.codigoBarra
 
-	select ol.id, count(pe.id) as cantidadDetalle into #ordenLocalDetalle from OrdenesLocales ol
-	left join PoEncabezado pe on ol.id = pe.idOrdenLocal
-	inner join #tempOrdenLocal temp on ol.id = temp.idOrdenLocal
-	group by ol.id
+    SELECT pe.idOrdenLocal INTO #tempOrdenLocal FROM PoEncabezado pe
+    INNER JOIN #tempPoEncabezado tabTemp ON pe.id = tabTemp.idPo
 
-	delete pe from PoEncabezado pe
-	inner join #poEncabezadoDetalle temp on pe.id = temp.id
-	where temp.cantidadDetalle=0
+    DELETE pd FROM PoDetalles pd
+    INNER JOIN @table tabTemp ON pd.codigoBarra = tabTemp.codigoBarra
 
-	delete ol from OrdenesLocales ol
-	inner join #ordenLocalDetalle temp on ol.id = temp.id
-	where temp.cantidadDetalle=0
+    --Eliminacion encabezados
+    SELECT pe.id, COUNT(pd.id) AS cantidadDetalle INTO #poEncabezadoDetalle FROM PoEncabezado pe
+    LEFT JOIN PoDetalles pd ON pe.id = pd.idPo
+    INNER JOIN #tempPoEncabezado temp ON pe.id = temp.idPo
+    GROUP BY pe.id
 
-	COMMIT TRANSACTION EliminaRegistros;
+    SELECT ol.id, COUNT(pe.id) AS cantidadDetalle INTO #ordenLocalDetalle FROM OrdenesLocales ol
+    LEFT JOIN PoEncabezado pe ON ol.id = pe.idOrdenLocal
+    INNER JOIN #tempOrdenLocal temp ON ol.id = temp.idOrdenLocal
+    GROUP BY ol.id
 
-	drop table #tempGuiaHouse
-	drop table #tempPoEncabezado
-	drop table #poEncabezadoDetalle
-	drop table #ordenLocalDetalle
+    DELETE pe FROM PoEncabezado pe
+    INNER JOIN #poEncabezadoDetalle temp ON pe.id = temp.id
+    WHERE temp.cantidadDetalle = 0
+
+    DELETE ol FROM OrdenesLocales ol
+    INNER JOIN #ordenLocalDetalle temp ON ol.id = temp.id
+    WHERE temp.cantidadDetalle = 0
+
+    COMMIT TRANSACTION EliminaRegistros;
+
+    DROP TABLE #tempGuiaHouse
+    DROP TABLE #tempPoEncabezado
+    DROP TABLE #poEncabezadoDetalle
+    DROP TABLE #ordenLocalDetalle
 
 END TRY
 BEGIN CATCH
-	ROLLBACK TRANSACTION EliminaRegistros;
-	EXEC [dbo].[pro_LogError] 
+    ROLLBACK TRANSACTION EliminaRegistros;
+    EXEC [dbo].[pro_LogError]
 END CATCH
 END
